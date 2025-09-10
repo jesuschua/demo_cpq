f05f874aba46aaef58f914c44cf960b47451af3c
jhc <jhc@gmail.com>
2025-09-10 02:03:13 +0200

    streamlined tests

--- playwright.regression.config.js
+++ playwright.regression.config.js
@@ -23,6 +23,9 @@ module.exports = defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:3000',
 
+    /* Run tests in headed mode by default, unless explicitly set to headless */
+    headless: process.env.HEADLESS === 'true' ? true : false,
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     
